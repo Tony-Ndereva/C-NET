4404def1f16ff057671330bd458937b41a42ac92
Tony <tonymaluki@gmail.com>
2023-09-01 12:46:21 +0300

    Add project files.

---
+++ Array/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_NET.Array
+{
+    public class ArrayPractise
+    {
+        public  void PrintArray()
+        {
+            var numbers = new int[3] { 1, 2, 3 };
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+        }
+       
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using C_NET.Array;
+using C_NET.Math;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace C_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            var practiseArray = new ArrayPractise();
+            practiseArray.PrintArray();
+        }
+    }
+}
