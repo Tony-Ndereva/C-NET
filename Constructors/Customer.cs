39dbe2b27db8e82a2cae1a152086d5795f03257e
Tony <tonymaluki@gmail.com>
2023-09-07 23:16:46 +0300

    using this

--- Constructors/Customer.cs
+++ Constructors/Customer.cs
@@ -9,15 +9,17 @@ namespace C_NET.Constructors
         public List<Order> Orders;
         public Customer()
         {
-            //Orders = new List<Order>();
+            Orders = new List<Order>();
         }
         public Customer(int id)
+            :this()
         {
             this.Id = id;
         }
         public Customer(int id, string name)
+            :this(id)
         {
-            this.Id = id;
+            
             this.Name = name;
         }
     }
