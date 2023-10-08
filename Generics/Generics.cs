1371c9b15b8be9542e826deb3af753b1f9611a65
tmaluki <tmaluki@seaburysolutions.com>
2023-10-08 20:36:17 +0300

    generics

---
+++ Generics/Generics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_NET.Generics
+{
+    public  class Iterate
+    {
+
+        
+        public static void displayElement<T>(T[] array)
+        {
+            foreach(var item in array)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
